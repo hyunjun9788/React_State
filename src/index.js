546358cc9089020d0775047f5be079382169910c
hyunjun9788 <namhyunjun9788@gmail.com>
2023-06-23 18:21:33 +0900

    feat: index.js 수정

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NotificationList from "./NotificationList";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NotificationList />
   </React.StrictMode>
 );
 
